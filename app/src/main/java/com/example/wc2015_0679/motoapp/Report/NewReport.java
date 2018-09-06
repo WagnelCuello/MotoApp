66b67477375a401541850530c45de3f4a501854b
wagnel <wagnelcuello@gmail.com>
2018-09-06 16:52:56 -0400

    Avance del dia de hoy 6-9-18

--- app/src/main/java/com/example/wc2015_0679/motoapp/Report/NewReport.java
+++ app/src/main/java/com/example/wc2015_0679/motoapp/Report/NewReport.java
@@ -1,40 +1,50 @@
 package com.example.wc2015_0679.motoapp.Report;
 
+import android.Manifest;
 import android.app.AlertDialog;
-import android.content.Context;
 import android.content.Intent;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.icu.util.DateInterval;
-import android.net.Uri;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.DialogFragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.DatePicker;
-import android.widget.Spinner;
-import android.widget.Toast;
+import android.widget.TextView;
 
 import com.example.wc2015_0679.motoapp.R;
-
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStream;
-import java.text.DateFormat;
+import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.MapsInitializer;
+import com.google.android.gms.maps.model.LatLng;
 
 public class NewReport extends DialogFragment {
     private Button btnUploadImg;
-    private Spinner spYearMoto;
-    private Integer[] years;
+    private TextView tvDateLost;
+    private MapView mapView;
+    private GoogleMap map;
     private static final int ACTIVITY_SELECT_IMAGE = 128;
+    private static final String MAP_VIEW_BUNDLE_KEY = "MapViewBundleKey";
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_new_report, container, false);
+
+        tvDateLost = rootView.findViewById(R.id.tvDateLost);
+        tvDateLost.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder alert = new AlertDialog.Builder(getContext());
+                alert.setTitle("Select Date Motorcycle Lost");
+                alert.setMessage("//TODO datepicker here");
+                alert.create().show();
+            }
+        });
+
         btnUploadImg = rootView.findViewById(R.id.btnUploadImg);
         btnUploadImg.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -44,13 +54,44 @@ public class NewReport extends DialogFragment {
                 startActivityForResult(galleryIntent, ACTIVITY_SELECT_IMAGE);
             }
         });
-        spYearMoto = rootView.findViewById(R.id.spYearMoto);
-        for (int i = 2000; i <= 2018; i++){
-            years[i] = i;
-        }
 
-        spYearMoto.setAdapter(new ArrayAdapter<Integer>(getContext(), android.R.layout.simple_spinner_dropdown_item, years));
+        // Gets the MapView from the XML layout and creates it
+        mapView = (MapView) rootView.findViewById(R.id.mapView);
+        mapView.onCreate(savedInstanceState);
+
+        // Gets to GoogleMap from the MapView and does initialization stuff
+        //map = mapView.getMap();
+        map.getUiSettings().setMyLocationButtonEnabled(false);
+        if (ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            //return TODO;
+        }
+        map.setMyLocationEnabled(true);
+
+        // Needs to call MapsInitializer before doing any CameraUpdateFactory calls
+        MapsInitializer.initialize(this.getActivity());
+
+        // Updates the location and zoom of the MapView
+        CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngZoom(new LatLng(43.1, -87.9), 10);
+        map.animateCamera(cameraUpdate);
+
         return rootView;
     }
-}
 
+    @Override
+    public void onResume() {
+        mapView.onResume();
+        super.onResume();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mapView.onDestroy();
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mapView.onLowMemory();
+    }
+}
